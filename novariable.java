a781bc5e1c5cfc9fe454c5509aa7336f20b4ad69
r-uday-kiran <udaykiranraikanti111@gmail.com>
2025-03-15 11:42:45 +0530

    Add files via upload

---
+++ novariable.java
@@ -0,0 +1,11 @@
+class novariable 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("-3.6");
+		System.out.println("Happy Birthday");
+		System.out.println("97");
+		System.out.println('@');
+		System.out.println("67@abc");
+	}
+}
