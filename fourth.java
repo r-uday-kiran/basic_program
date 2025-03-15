c149148b5ca5fae1e7dc133b5539145a293f5a94
r-uday-kiran <udaykiranraikanti111@gmail.com>
2025-03-15 11:40:35 +0530

    Add files via upload

---
+++ fourth.java
@@ -0,0 +1,16 @@
+class fourth 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("@");
+		System.out.println("$");
+		System.out.println("T");
+		System.out.println('6');
+		System.out.println("P");
+		System.out.println("z");
+		System.out.println(' ');
+		System.out.println("H");
+		System.out.println('0');
+		System.out.println("#");
+	}
+}
